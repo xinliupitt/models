c51fc64dc927b55ddf0c7c66cf3aa0fae1c4773d
xinliupitt <xinliupitt@gmail.com>
2020-07-30 17:31:28 -0400

    input change

--- official/nlp/transformer/transformer.py
+++ official/nlp/transformer/transformer.py
@@ -21,7 +21,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-workon_new = True
+workon_new = False
 
 import numpy as np
 
--- official/nlp/transformer/transformer_forward.py
+++ official/nlp/transformer/transformer_forward.py
@@ -27,8 +27,8 @@ import tensorflow as tf
 from official.nlp.transformer import model_params
 from official.nlp.transformer import transformer
 
-is_train = False
-get_weights_flag = False
+is_train = True
+get_weights_flag = True
 
 def _count_params(layer, trainable_only=True):
   """Returns the count of all model parameters, or just trainable ones."""
@@ -82,7 +82,7 @@ class TransformerV2Test(tf.test.TestCase):
       model = transformer.create_model(self.params, True)
       # inputs = np.asarray([[5, 2, 1], [7, 5, 0], [1, 4, 0], [7, 5, 11]])
       # targets = np.asarray([[4, 3, 0], [13, 19, 17], [20, 14, 1], [5, 7, 0]])
-      inputs = np.asarray([[5, 2, 1], [7, 5, 2], [1, 4, 9], [7, 5, 11]])
+      inputs = np.asarray([[5, 2], [7, 5], [1, 4], [7, 5]])
       targets = np.asarray([[4, 3, 5], [13, 19, 17], [20, 14, 1], [5, 7, 8]])
       w = load('w.npy', allow_pickle=True)
       model.set_weights(w)
