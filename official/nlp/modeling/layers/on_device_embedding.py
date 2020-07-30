fee5b64121f9ed7592c61cb03adc4d32834f4a50
xinliupitt <xinliupitt@gmail.com>
2020-07-30 17:08:15 -0400

    initializer

--- official/nlp/modeling/layers/on_device_embedding.py
+++ official/nlp/modeling/layers/on_device_embedding.py
@@ -67,7 +67,8 @@ class OnDeviceEmbedding(tf.keras.layers.Layer):
     self.embeddings = self.add_weight(
         "embeddings",
         shape=[self._vocab_size, self._embedding_width],
-        initializer=self._initializer,
+        initializer=tf.random_normal_initializer(
+              mean=0., stddev=self.hidden_size**-0.5, seed=1234),
         dtype=tf.float32)
 
     super(OnDeviceEmbedding, self).build(input_shape)
